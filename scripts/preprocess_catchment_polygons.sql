fd961df26270b07ce9bf9d50d732b17e18724abe
Santiago Palladino <spalladino@gmail.com>
2016-06-23 16:10:29 -0300

    Choose method for precalculating isochrones
    - Add method column to facilities_polygons
    - Add notice for each facility being processed

--- scripts/preprocess_catchment_polygons.sql
+++ scripts/preprocess_catchment_polygons.sql
@@ -11,6 +11,7 @@ drop table if exists facilities_polygons;
 create table facilities_polygons (
         facility_id integer not null,
         threshold   integer not null,
+        method      varchar not null,
         the_geom    geometry(polygon, 4326)
 );
 
@@ -24,13 +25,13 @@ begin
   from ways_vertices_pgr w
   order by w.the_geom <-> original limit 1
   into closest_node;
-  
+
   return closest_node;
 end;
 $$ language plpgsql;
 
 -- generates the isochrone polygons for all the facilities and thresholds
-create or replace function calculate_isochrones(threshold_start integer, threshold_finish integer, threshold_jump integer)
+create or replace function calculate_isochrones(method varchar, threshold_start integer, threshold_finish integer, threshold_jump integer)
 returns void as $$
 declare
   from_cost integer;
@@ -44,6 +45,8 @@ begin
 
   -- Process only facilities with 'hospital' in their name
   for f_row in select * from facilities f where name ilike '%hospital%' loop
+    RAISE NOTICE 'Processing %', f_row.id;
+
     insert into edges_agg_cost (
       select e.edge, e.agg_cost
       from pgr_drivingdistance(
@@ -56,23 +59,28 @@ begin
     from_cost := 0;
     to_cost   := threshold_start * 60;
     while to_cost <= threshold_finish * 60 loop
-      insert into facilities_polygons (
-        select f_row.id, to_cost, st_union(buffers.the_geom)
-        from (
-          select wb.the_geom
-          from edges_agg_cost eac
-          join ways_buffers wb on wb.ways_gid = eac.gid
-          where agg_cost >= from_cost and agg_cost < to_cost
-          union
-          select the_geom
-          from facilities_polygons
-          where facility_id = f_row.id and threshold = (to_cost-threshold_jump * 60)
-        ) as buffers
-      );
-      -- Uncomment the following code to use the faster (but inaccurate) alpha
-      -- shape method
-      -- insert into facilities_polygons (
-      --        select f_row.id, to_cost, st_buffer(st_setsrid(pgr_pointsaspolygon('select id::integer, lon::float as x, lat::float as y from ways_nodes where gid in (select gid from edges_agg_cost where agg_cost < ' || to_cost || ')'),4326), 0.004));
+
+      IF method = 'buffer' THEN
+        insert into facilities_polygons (
+          select f_row.id, to_cost, method, st_union(buffers.the_geom)
+          from (
+            select wb.the_geom
+            from edges_agg_cost eac
+            join ways_buffers wb on wb.ways_gid = eac.gid
+            where agg_cost >= from_cost and agg_cost < to_cost
+            union
+            select the_geom
+            from facilities_polygons
+            where facility_id = f_row.id and threshold = (to_cost-threshold_jump * 60)
+          ) as buffers
+        );
+      ELSIF method = 'alpha-shape' THEN
+        insert into facilities_polygons (
+               select f_row.id, to_cost, method, st_buffer(st_setsrid(pgr_pointsaspolygon('select id::integer, lon::float as x, lat::float as y from ways_nodes where gid in (select gid from edges_agg_cost where agg_cost < ' || to_cost || ')'),4326), 0.004));
+      ELSE
+        RAISE EXCEPTION 'Method % unknown', method USING HINT = 'Please use buffer or alpha-shape';
+      END IF;
+
       from_cost := to_cost;
       to_cost   := to_cost + threshold_jump * 60;
     end loop;
