58b47c305e1a2c0b305fc5f13cd5a2fac946d784
Santiago Palladino <spalladino@gmail.com>
2016-07-12 16:51:38 -0300

    Return proper not-found response in project endpoint

--- src/planwise/endpoint/projects.clj
+++ src/planwise/endpoint/projects.clj
@@ -1,6 +1,6 @@
 (ns planwise.endpoint.projects
   (:require [compojure.core :refer :all]
-            [ring.util.response :refer [content-type response]]
+            [ring.util.response :refer [content-type response not-found]]
             [clojure.data.json :as json]
             [planwise.boundary.projects :as projects]))
 
@@ -14,7 +14,7 @@
     (GET "/:id/details" [id]
       (if-let [project (projects/get-project service (Integer/parseInt id))]
         (response project)
-        (throw "not-found")))
+        (not-found {:error "Project not found"})))
 
     (POST "/" [goal]
       (response (projects/create-project service {:goal goal})))))
