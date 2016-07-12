6b290ad0b8795d14d17134a3fcb3702bf925aa87
Gustavo Giráldez <ggiraldez@manas.com.ar>
2016-07-12 18:56:20 -0300

    Fix test

--- test/planwise/endpoint/home_test.clj
+++ test/planwise/endpoint/home_test.clj
@@ -14,10 +14,17 @@
                  "/projects/1/transport"
                  "/projects/1/scenarios"])
 
+(deftest home-endpoint-checks-login
+  (doseq [path home-paths]
+    (testing (str "path " path " throws 401")
+      (is (thrown? clojure.lang.ExceptionInfo
+                   (-> (session handler)
+                       (visit path)))))))
+
 (deftest root-page-test
   (doseq [path home-paths]
     (testing (str "path " path " exists and renders CLJS application")
-     (-> (session handler)
-         (visit path)
-         (has (status? 200))
-         (has (some-text? "Loading Application"))))))
+      (-> (session handler)
+          (visit path :identity {:user "foo@example.com"})
+          (has (status? 200))
+          (has (some-text? "Loading Application"))))))
