cd476e83c45b9ac4d9ef334218e385152426faf1
Santiago Palladino <spalladino@gmail.com>
2016-07-19 17:49:07 -0300

    Return facilities with isochrones with filter criteria
    Endpoint `/with-isochrones` accepts and uses the same filter criteria (by region and by type) as the list endpoint.

--- resources/planwise/sql/facilities.sql
+++ resources/planwise/sql/facilities.sql
@@ -12,24 +12,31 @@ SELECT
 FROM facilities
 ORDER BY name;
 
+-- :snip criteria-snip
+/*~ (if (:types params) */
+  AND facilities.type IN (:v*:types)
+/*~ ) ~*/
+/*~ (if (:region params) */
+  AND facilities.the_geom @ (SELECT the_geom FROM regions WHERE id = :region LIMIT 1)
+  AND ST_Contains((SELECT the_geom FROM regions WHERE id = :region LIMIT 1), facilities.the_geom)
+/*~ ) ~*/
+
 -- :name facilities-by-criteria :? :*
 SELECT
 id, name, type, lat, lon
 FROM facilities
-WHERE type IN (:v*:types)
-/*~ (if (:region params) */
-  AND ST_Contains((SELECT the_geom FROM regions WHERE id = :region LIMIT 1), facilities.the_geom)
-/*~ ) ~*/
-ORDER BY name;
+WHERE 1=1
+:snip:criteria ;
 
 -- :name facilities-with-isochrones :?
 SELECT
-  fp.facility_id, f.name, f.lat, f.lon, ST_AsGeoJSON(ST_Simplify(fp.the_geom, :simplify)) AS isochrone
+  facilities.id AS id, facilities.name AS name, facilities.lat AS lat, facilities.lon AS lon,
+  ST_AsGeoJSON(ST_Simplify(fp.the_geom, :simplify)) AS isochrone
 FROM facilities_polygons fp
-INNER JOIN facilities f
-ON fp.facility_id = f.id
+  INNER JOIN facilities ON fp.facility_id = facilities.id
 WHERE fp.threshold = :threshold
-AND fp.method = :method;
+  AND fp.method = :algorithm
+  :snip:criteria ;
 
 -- :name isochrone-for-facilities :? :1
 SELECT
