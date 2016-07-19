56c42f69250d0f75066461cf9a44b89d4a93a82c
Gustavo Giráldez <ggiraldez@manas.com.ar>
2016-07-19 12:15:06 -0300

    Handle OAuth2 authorization denied.

--- src/planwise/endpoint/resmap_auth.clj
+++ src/planwise/endpoint/resmap_auth.clj
@@ -1,5 +1,6 @@
 (ns planwise.endpoint.resmap-auth
   (:require [compojure.core :refer :all]
+            [taoensso.timbre :as timbre]
             [ring.util.response :refer [redirect response content-type]]
             [hiccup.page :refer [html5]]
             [buddy.auth :refer [throw-unauthorized authenticated?]]
@@ -8,9 +9,15 @@
             [planwise.component.auth :as auth]
             [planwise.component.resmap :as resmap]))
 
+(timbre/refer-timbre)
+
 (def oauth2-callback-page
   (html5
    [:script "window.opener.postMessage('authenticated', window.location.origin);window.close();"]))
+
+(def oauth2-callback-error-page
+  (html5
+   [:script "window.close();"]))
 
 (defn resmap-auth-routes
   [{:keys [auth resmap]}]
@@ -20,14 +27,19 @@
            return-url (absolute-url "/oauth2/callback" req)
            authorisation-url (auth/oauth2-authorization-url auth scope return-url)]
        (redirect authorisation-url)))
-   (GET "/callback" [code :as req]
-     (let [scope (resmap/auth-scope resmap)
-           return-url (absolute-url "/oauth2/callback" req)
-           token (auth/oauth2-fetch-token auth code return-url)
-           user (:identity req)]
-       (auth/save-auth-token! auth scope user token)
-       (-> (response oauth2-callback-page)
-           (content-type "text/html"))))))
+   (GET "/callback" [code error :as req]
+     (if error
+       (do
+         (info "OAuth2 authorization denied with error" error)
+         (-> (response oauth2-callback-error-page)
+             (content-type "text/html")))
+       (let [scope (resmap/auth-scope resmap)
+             return-url (absolute-url "/oauth2/callback" req)
+             token (auth/oauth2-fetch-token auth code return-url)
+             user (:identity req)]
+         (auth/save-auth-token! auth scope user token)
+         (-> (response oauth2-callback-page)
+             (content-type "text/html")))))))
 
 (defn resmap-auth-endpoint
   [system]
