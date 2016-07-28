74a2ed48d0b023676f4ad900897bb88b98db5942
Gustavo Giráldez <ggiraldez@manas.com.ar>
2016-07-28 16:00:54 -0300

    Fix apply traffic factor migration.

--- resources/migrations/015-apply_traffic_factor.down.sql
+++ resources/migrations/015-apply_traffic_factor.down.sql
@@ -1,4 +1,4 @@
 UPDATE ways
-       SET cost_s = length_m / maxspeed_forward * 5 / 18 * 1.0;
+       SET cost_s = length_m / (maxspeed_forward * 5 / 18) * 1.0;
 
 DROP FUNCTION IF EXISTS apply_traffic_factor(float);
