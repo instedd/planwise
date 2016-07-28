9a57a99efdf20ad88e86b9c65668b427f7c9dc48
Gustavo Giráldez <ggiraldez@manas.com.ar>
2016-07-28 13:40:41 -0300

    Apply traffic factor to routing information.
    Increase the cost of all the ways by 50%. Also install a PLSQL function
    to update the factor further.
    
    Fixes #80.

---
+++ resources/migrations/015-apply_traffic_factor.up.sql
@@ -0,0 +1,10 @@
+CREATE OR REPLACE FUNCTION apply_traffic_factor(factor float)
+RETURNS void AS $$
+BEGIN
+  UPDATE ways
+         SET cost_s = length_m / maxspeed_forward * 5 / 18 * factor;
+END;
+$$ LANGUAGE plpgsql;
+
+UPDATE ways
+       SET cost_s = length_m / maxspeed_forward * 5 / 18 * 1.5;
