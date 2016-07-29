54e3b9ed945464a48804bff99db86144377ed272
Santiago Palladino <spalladino@gmail.com>
2016-07-29 10:24:56 -0300

    Add regions method for loading either with preview or with full geo
    Preview to be used in projects listing, while full geo in new project preview or project page. Note that full geo is not simplified for now, as the largest GeoJSON (Kenya) is just about 130kb.
    
    Also rename admin_level to admin-level in region model.

--- src/planwise/boundary/regions.clj
+++ src/planwise/boundary/regions.clj
@@ -7,8 +7,11 @@
   (list-regions [this]
     "Returns all regions in the database.")
 
+  (list-regions-with-preview [this ids]
+    "Returns regions including a preview geojson of their boundaries given their ids.")
+
   (list-regions-with-geo [this ids simplify]
-    "Returns regions including a simplified geojson with ther boundaries given their ids."))
+    "Returns regions including a simplified geojson with their boundaries given their ids."))
 
 ;; Reference implementation
 
@@ -16,5 +19,7 @@
   planwise.component.regions.RegionsService
   (list-regions [service]
     (service/list-regions service))
+  (list-regions-with-preview [service ids]
+    (service/list-regions-with-preview service ids))
   (list-regions-with-geo [service ids simplify]
     (service/list-regions-with-geo service ids simplify)))
--- src/planwise/component/regions.clj
+++ src/planwise/component/regions.clj
@@ -2,6 +2,7 @@
   (:require [clojure.java.jdbc :as jdbc]
             [com.stuartsierra.component :as component]
             [clojure.data.json :as json]
+            [clojure.set :as set]
             [hugsql.core :as hugsql]))
 
 ;; ----------------------------------------------------------------------
@@ -12,9 +13,12 @@
 (defn get-db [service]
   (get-in service [:db :spec]))
 
-(defn wrap-region [{json-bbox :bbox, :as region}]
+(defn db->region [{json-bbox :bbox, :as record}]
   (let [[se ne nw sw se'] (map reverse (get-in (json/read-str json-bbox) ["coordinates" 0]))]
-    (assoc region :bbox [sw ne])))
+    (-> record
+      (assoc :bbox [sw ne])
+      (set/rename-keys {:admin_level :admin-level
+                        :preview_geojson :preview-geojson}))))
 
 
 ;; ----------------------------------------------------------------------
@@ -32,9 +36,13 @@
 ;; Service functions
 
 (defn list-regions [service]
-  (map wrap-region
+  (map db->region
     (select-regions (get-db service))))
 
+(defn list-regions-with-preview [service ids]
+  (map db->region
+    (select-regions-with-preview-given-ids (get-db service) {:ids ids})))
+
 (defn list-regions-with-geo [service ids simplify]
-  (map wrap-region
+  (map db->region
     (select-regions-with-geo-given-ids (get-db service) {:ids ids, :simplify simplify})))
