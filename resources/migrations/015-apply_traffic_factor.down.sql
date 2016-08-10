58e29a2d2fc1c870ec2529648dfb62cc40a7257f
Santiago Palladino <spalladino@gmail.com>
2016-08-10 15:44:56 -0300

    Fix in down migration for apply traffic factor

--- resources/migrations/015-apply_traffic_factor.down.sql
+++ resources/migrations/015-apply_traffic_factor.down.sql
@@ -1,4 +1,8 @@
-IF EXISTS (SELECT 1 FROM pg_class WHERE relname = 'ways') THEN
-  UPDATE ways
-  SET cost_s = length_m / (maxspeed_forward * 5 / 18) * 1.0;
-END IF;
+DO $$
+  BEGIN
+    IF EXISTS (SELECT 1 FROM pg_class WHERE relname = 'ways') THEN
+      UPDATE ways
+      SET cost_s = length_m / (maxspeed_forward * 5 / 18) * 1.0;
+    END IF;
+  END;
+$$
