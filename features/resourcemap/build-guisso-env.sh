c583460a8614e2d856046e78e66d8df19098109e
Brian J. Cardiff <bcardiff@manas.com.ar>
2016-08-12 14:27:16 -0300

    remove .env-e file while building guisso-env
    add features/*/guisso.env to .gitignore

--- features/resourcemap/build-guisso-env.sh
+++ features/resourcemap/build-guisso-env.sh
@@ -3,3 +3,4 @@ GUISSO_ENV_FILE="${0%/*}/guisso.env"
 docker-compose -f "${0%/*}/../guisso/docker-compose.yml" run --rm guissoweb rake apps:create[resmap,resmapweb:5080,trusted] > $GUISSO_ENV_FILE
 sed -i -e 's/^client_id:\ /GUISSO_CLIENT_ID=/g' $GUISSO_ENV_FILE
 sed -i -e 's/^client_secret:\ /GUISSO_CLIENT_SECRET=/g' $GUISSO_ENV_FILE
+rm "$GUISSO_ENV_FILE-e"
