4118594d9df3b97b404e5ad4758de8d962c37095
Gustavo Giráldez <ggiraldez@manas.com.ar>
2016-08-17 10:08:19 -0300

    Reimplement import status reporting in the client.
    Use the new reported status from the server endpoint.
    Make it more robust to race conditions and certain concurrency
    issues (fixes #94).

--- src/planwise/client/datasets/db.cljs
+++ src/planwise/client/datasets/db.cljs
@@ -1,4 +1,26 @@
-(ns planwise.client.datasets.db)
+(ns planwise.client.datasets.db
+  (:require [schema.core :as s :include-macros true]))
+
+(s/defschema ServerStatus
+  {:status                    (s/enum :ready :done :importing :cancelling :unknown)
+   (s/optional-key :state)    s/Keyword
+   (s/optional-key :result)   s/Any
+   (s/optional-key :progress) s/Any})
+
+(s/defschema Datasets
+  "Datasets related portion of the client database"
+  {:state          (s/enum nil
+                           :initialising
+                           :ready
+                           :import-requested
+                           :importing
+                           :cancel-requested
+                           :cancelling)
+   :server-status  (s/maybe ServerStatus)
+   :facility-count (s/maybe s/Int)
+   :resourcemap    {:authorised? (s/maybe s/Bool)
+                    :collections s/Any}
+   :selected       s/Any})
 
 (def empty-datasets-selected
   {:collection nil
@@ -11,17 +33,78 @@
                                         ; Field selected for mapping facility type
 
 (def initial-db
-  {:state nil
+  {:state          nil
                                         ; :initialising/nil :ready :importing
-   :cancel-requested false
-   :raw-status       nil
-   :facility-count   nil
+   :server-status  nil
+                                        ; Importer status as reported by the server
+   :facility-count nil
                                         ; Count of available facilities
-   :resourcemap {
-                 :authorised?  nil
+   :resourcemap    {:authorised?  nil
                                         ; Whether the user has authorised for
                                         ; Resourcemap access
-                 :collections  nil}
+                    :collections  nil}
                                         ; Resourcemap collections
 
    :selected empty-datasets-selected})
+
+
+(defn initialised?
+  [state]
+  (and (not= :initialising state)
+       (not (nil? state))))
+
+(defn importing?
+  [state]
+  (or (= :importing state)
+      (= :import-requested state)
+      (= :cancelling state)
+      (= :cancel-requested state)))
+
+(defn cancelling?
+  [state]
+  (or (= :cancelling state)
+      (= :cancel-requested state)))
+
+(defn request-pending?
+  [state]
+  (or (= :cancel-requested state)
+      (= :import-requested state)))
+
+(defn server-status->string
+  [server-status]
+  (case (:status server-status)
+    :ready
+    "Ready"
+
+    :done
+    (case (:result server-status)
+      :success "Import successful"
+      :cancelled "Import cancelled"
+      :unexpected-event "Fatal error: unexpected event received"
+      :import-types-failed "Error: failed to import facility types"
+      :import-sites-failed "Error: failed to import sites from Resourcemap"
+      :update-projects-failed "Error: failed to update projects")
+
+    :importing
+    (let [progress (:progress server-status)]
+      ;; TODO: add progress to the returned string
+      (case (:state server-status)
+        (:start :importing-types) "Importing facility types"
+        (:request-sites :importing-sites) "Importing sites from Resourcemap"
+        (:processing-facilities) "Pre-processing facilities"
+        (:update-projects :updating-projects) "Updating projects"
+        "Importing..."))
+
+    :cancelling
+    "Cancelling..."
+
+    :unknown
+    "Unknown server status"))
+
+(defn server-status->state
+  [{status :status}]
+  (case status
+    (:ready :done) :ready
+    :importing     :importing
+    :cancelling    :cancelling
+    :unknown       :ready))
