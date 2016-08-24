abd0cf71179042cc2e2965622bc358d096a54eb6
Mauri <mmuller@manas.com.ar>
2016-08-24 17:16:31 -0300

    Added login test

--- features/spec/login_spec.rb
+++ features/spec/login_spec.rb
@@ -1,7 +1,13 @@
 describe Home do
-  it "should go to guisso for login" do
+  it "should login" do
     goto_page Home do
-      expect_page GuissoLogin
+      expect_page GuissoLogin do |page|
+      	page.form.user_name.set "admin@instedd.org"
+      	page.form.password.set "admin123"
+      	page.form.login.click
+      end
     end
+
+    expect(page).to have_content 'Projects'
   end
 end
--- features/spec/support/page_model/guisso_login.rb
+++ features/spec/support/page_model/guisso_login.rb
@@ -1,3 +1,9 @@
 class GuissoLogin < SitePrism::Page
   set_url "http://guisso-local.instedd.org/users/sign_in"
+
+  section :form, '#new_user' do
+    element :user_name, '#user_email'
+    element :password, '#user_password'
+    element :login, '[name="commit"]'
+  end
 end
