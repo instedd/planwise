65e3df8b00c16ffbd66016f9194220ff8e1ee196
mmuller <mmuller@manas.com.ar>
2016-08-30 16:52:41 -0300

    Added 'open_project_view' method

--- features/spec/project_spec.rb
+++ features/spec/project_spec.rb
@@ -6,14 +6,14 @@ describe "Project" do
 
   it "should search project" do
     goto_page HomePage do |page|  
-      fill_in  "search", :with => "Foo"
+      fill_in "search", :with => "Foo"
     end 
   end
 
   it "should delete a project" do
     goto_page HomePage do |page|
       expect(page).to have_content("Foo")
-      page.find(".project-card").click
+      open_project_view
     end
     expect_page ProjectPage do |page|
       page.delete.click
