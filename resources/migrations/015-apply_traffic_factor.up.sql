986697fd8562d1dab9d9fdba38604f4f60831a2c
Juan Edi <jedi@manas.com.ar>
2016-09-01 16:12:41 -0300

    #142 Fix: use float division when applyting traffic factor

--- resources/migrations/015-apply_traffic_factor.up.sql
+++ resources/migrations/015-apply_traffic_factor.up.sql
@@ -2,7 +2,7 @@ DO $$
   BEGIN
     IF EXISTS (SELECT 1 FROM pg_class WHERE relname = 'ways') THEN
       UPDATE ways
-      SET cost_s = length_m / (maxspeed_forward * 5 / 18) * 1.5;
+      SET cost_s = length_m / (maxspeed_forward * 5.0 / 18) * 1.5;
     END IF;
   END;
 $$
