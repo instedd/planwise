263c4561bfc74eabc349cb6b4af5c506592c09fe
mmuller <mmuller@manas.com.ar>
2016-09-07 14:27:16 -0300

    Added 'signout' method

--- features/spec/support/page_model/home_page.rb
+++ features/spec/support/page_model/home_page.rb
@@ -2,6 +2,7 @@ class HomePage < SitePrism::Page
   set_url "/"
 
   element :primary, ".primary"
+  element :signout, ".signout"
 
   def press_primary_button
     primary.click
@@ -10,5 +11,10 @@ class HomePage < SitePrism::Page
 
   def wait_for_submit
     sleep 0.5
+  end
+
+  def press_signout_button
+    signout.click
+    wait_for_submit
   end
 end
