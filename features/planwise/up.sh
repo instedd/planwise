2a053288bc1fe35f84c86b1281f26660195ce564
mmuller <mmuller@manas.com.ar>
2016-09-20 16:32:51 -0300

    Specified region for integration tests

--- features/planwise/up.sh
+++ features/planwise/up.sh
@@ -3,5 +3,5 @@ ${0%/*}/build-guisso-env.sh
 docker-compose -f "${0%/*}/docker-compose.yml" up -d planwisedb
 sleep 10
 # setup database
-docker-compose -f "${0%/*}/docker-compose.yml" run --rm planwiseweb /bin/sh -c "sleep 5 && cd /app && scripts/migrate && scripts/import-osm && scripts/load-regions"
+docker-compose -f "${0%/*}/docker-compose.yml" run --rm planwiseweb /bin/sh -c "sleep 5 && cd /app && scripts/migrate && scripts/import-osm osm2pgrouting kenya && scripts/load-regions kenya"
 docker-compose -f "${0%/*}/docker-compose.yml" up -d
