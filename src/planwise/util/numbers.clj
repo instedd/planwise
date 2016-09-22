eb724072e78c49901e32feeee97c96fba205a79e
Santiago Palladino <spalladino@gmail.com>
2016-09-22 19:29:49 -0300

    Avoid exact comparison for facilities latlng in import process
    Use epsilon comparison based on method from http://gettingclojure.wikidot.com/cookbook:numbers

---
+++ src/planwise/util/numbers.clj
@@ -0,0 +1,21 @@
+(ns planwise.util.numbers)
+
+(defn float=
+
+  ([x y]
+   (float= x y 0.00001))
+
+  ([x y epsilon]
+   (cond
+
+    (and (nil? x) (nil? y))
+    true
+
+    (and x y)
+    (let [x (float x)
+          y (float y)
+          scale (if (or (zero? x) (zero? y)) 1 (Math/abs x))]
+      (<= (Math/abs (- x y)) (* scale epsilon)))
+
+    :else
+    false)))
