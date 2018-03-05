52bd51d19b38aa06b9225948807582a17412512f
Franciscello <ftarulla@gmail.com>
2018-03-05 16:27:15 -0300

    Add insert (and query) population sources

--- scripts/population/src/import.clj
+++ scripts/population/src/import.clj
@@ -10,6 +10,25 @@
               :user "planwise"
               :password "planwise" })
 
+(def sql-query
+  #(jdbc/query pg-db %))
+
+(def sql-insert! ; db-spec :table {:col1 42 :col2 "123"}
+  #(jdbc/insert! pg-db %1 %2))
+
+(defn add-population-source
+  [name, filename]
+  (sql-insert! :population_sources {:name name :tif_file filename}))
+
+;
+;
+(defn print-source
+  [source]
+  (println (:id source))
+  (println (:name source))
+  (println (:tif_file source)))
+
+
 (defn -main
   [name filename country]
 
@@ -19,7 +38,10 @@
 
   ; (println (:out (shell/sh "ls" "-al")))
 
-  (jdbc/query pg-db
-    ["select * from table where foo = ?" "bar"])
+  (let [ sources (sql-query ["select * from population_sources where id = ?" 1]) ]
+    (map print-source sources)
+  )
 
-  (System/exit 0))
+  (add-population-source name filename)
+
+  (System/exit 0)) ; https://stackoverflow.com/questions/1134770/how-to-end-force-a-close-to-a-program-in-clojure?rq=1
