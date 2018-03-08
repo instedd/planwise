bd51807bef1517b82d4f8d194e183fd8bef7e7fa
Franciscello <ftarulla@gmail.com>
2018-03-08 18:12:38 -0300

    Add raster-regions

--- scripts/population/src/import.clj
+++ scripts/population/src/import.clj
@@ -19,7 +19,7 @@
 (def run-script
   #(let [sh-result (apply shell/sh %1)]
       (println (:out sh-result))
-      ; (println (:err sh-result))
+      (println (:err sh-result))
       ))
 
 (def in?
@@ -46,13 +46,19 @@
   ; (println (:err (shell/sh "sh" "-c" (str "cd /app;" "./scripts/population/load-regions " country) )))
   )
 
-  (defn calculate-country-population
-    [country tif-filename-id]
-    (println (str "*****************************************************************"))
-    (println (str "* Running script to calculate population for: " country " with tif file id: " tif-filename-id))
-    (println (str "***"))
-    (run-script ["./regions-population" country (str tif-filename-id)]))
+(defn calculate-country-population
+  [country tif-filename-id]
+  (println (str "*****************************************************************"))
+  (println (str "* Running script to calculate population for: " country " with tif file id: " tif-filename-id))
+  (println (str "***"))
+  (run-script ["./regions-population" country (str tif-filename-id)]))
 
+(defn raster-all-regions
+  [country tif-filename-id]
+  (println (str "*****************************************************************"))
+  (println (str "* Running script to raster all regions from: " country))
+  (println (str "***"))
+  (run-script ["./raster-regions" country (str tif-filename-id)]))
 ;
 (defn print-source
   [source]
@@ -73,14 +79,14 @@
   (println (str ""))
 
   (when (in? "--build-cpp" options)
-    (build-cpp))
+    (build-cpp)) ;build programs in app/cpp/
 
   (let [ result (add-population-source name filename) ]
-    (doseq [r result]
+    (doseq [ret result]
       ; (print-source r)
       (add-country-regions country) ;load-regions
-      (calculate-country-population country (:id r)) ;regions-population
-      ; TODO: (rasterize-all-regions)
+      (calculate-country-population country (:id ret)) ;regions-population
+      (raster-all-regions country (:id ret)) ;raster-regions
     ))
 
   (println (str "All done!"))
