7ecdcb794c2975f6d3d865c1ff1b657e6288830f
Brian J. Cardiff <bcardiff@gmail.com>
2018-04-04 15:01:03 -0300

    Use asdf state to lookup for datasets

--- src/planwise/client/datasets2/components/dropdown.cljs
+++ src/planwise/client/datasets2/components/dropdown.cljs
@@ -1,20 +1,22 @@
 (ns planwise.client.datasets2.components.dropdown
-  (:require [re-frame.core :refer [subscribe dispatch]]
-            [reagent.core :as r]
-            [clojure.string :as string]
-            [planwise.client.utils :as utils]
-            [planwise.client.components.common2 :as common2]
-            [planwise.client.datasets2.db :as db]
-            [planwise.client.ui.rmwc :as m]
-            [re-frame.utils :as c]))
+    (:require [re-frame.core :refer [subscribe dispatch]]
+              [reagent.core :as r]
+              [clojure.string :as string]
+              [planwise.client.asdf :as asdf]
+              [planwise.client.utils :as utils]
+              [planwise.client.components.common2 :as common2]
+              [planwise.client.datasets2.db :as db]
+              [planwise.client.ui.rmwc :as m]
+              [re-frame.utils :as c]))
 
 (defn datasets-dropdown-component
   [{:keys [label value on-change]}]
-  (let [datasets-list (subscribe [:datasets2/dropdown-options])]
-    (dispatch [:datasets2/load-datasets2])
-    (fn [{:keys [label value on-change]}]
-      [m/Select {:label (if (empty? @datasets-list) "There are no datasets defined." label)
-                 :disabled (empty? @datasets-list)
-                 :value (str value)
-                 :options (sort-by :label @datasets-list)
-                 :on-change #(on-change (js/parseInt (-> % .-target .-value)))}])))
+  (let [datasets-sub     (subscribe [:datasets2/list])
+        datasets-options (subscribe [:datasets2/dropdown-options])]
+    (when (asdf/should-reload? @datasets-sub)
+      (dispatch [:datasets2/load-datasets2]))
+    [m/Select {:label (if (empty? @datasets-options) "There are no datasets defined." label)
+                :disabled (empty? @datasets-options)
+                :value (str value)
+                :options (sort-by :label @datasets-options)
+                :on-change #(on-change (js/parseInt (-> % .-target .-value)))}]))
