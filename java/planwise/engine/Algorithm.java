f87298b1c2d9101d67cbd82f4568a3dda98bffc6
Gustavo Giráldez <ggiraldez@manas.tech>
2018-04-12 17:36:04 -0300

    Classify population data using quartiles and output a renderable TIF

--- java/planwise/engine/Algorithm.java
+++ java/planwise/engine/Algorithm.java
@@ -1,5 +1,7 @@
 package planwise.engine;
 
+import java.util.Arrays;
+
 public class Algorithm {
     public static long countPopulation(float[] popData,
                                        float popNodata) {
@@ -82,6 +84,40 @@ public class Algorithm {
             }
             popIdx += popSkip;
             covIdx += covSkip;
+        }
+    }
+
+    public static float[] computeExactQuartiles(float[] data, float nodata) {
+        float copy[] = new float[data.length];
+        int j = 0;
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] != nodata) {
+                copy[j++] = data[i];
+            }
+        }
+        Arrays.sort(copy, 0, j);
+        return new float[]{ copy[0], copy[j/4], copy[j/2], copy[3*j/4], copy[j-1] };
+    }
+
+    public static void mapDataForRender(float[] popData,
+                                        float popNodata,
+                                        byte[] renderData,
+                                        byte renderNodata,
+                                        float[] quartiles) {
+        if (quartiles.length < 1 || quartiles.length > 128) {
+            throw new IllegalArgumentException("quartiles array has invalid size");
+        }
+        for (int i = 0; i < popData.length; i++) {
+            float value = popData[i];
+            byte renderValue = 0;
+            if (value == popNodata) {
+                renderValue = renderNodata;
+            } else {
+                while (renderValue < quartiles.length && value > quartiles[renderValue]) {
+                    renderValue++;
+                }
+            }
+            renderData[i] = renderValue;
         }
     }
 }
