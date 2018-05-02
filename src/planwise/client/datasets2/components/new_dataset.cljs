1aae847b666a1d4a890922ca6d92a475fb7c399f
Sol Sarratea <ssarratea@manas.tech>
2018-05-02 12:32:43 -0300

    Display correct file-name

--- src/planwise/client/datasets2/components/new_dataset.cljs
+++ src/planwise/client/datasets2/components/new_dataset.cljs
@@ -44,12 +44,12 @@
          (when (= @view-state open?)
            [:input {:id "file-upload"
                     :type "file"
+                    :class "file-input"
                     :value ""
                     :on-change  #(rf/dispatch [:datasets2/new-dataset-update
                                                :js-file (-> (.-currentTarget %) .-files (aget 0))])}])
-           (when (some? @js-file)
-              ;(println (-> @js-file .getName))
-              [:span (.-name @js-file)])]
+         (when (some? @js-file)
+           [:span (.-name @js-file)])]
         [:a {:href (routes/download-sample)
              :data-trigger "false"} "Download samples sites"]
         [m/Select {:label "Coverage algorithm"
