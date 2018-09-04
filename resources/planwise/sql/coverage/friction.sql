75bccfa1feb66d5f989a53be2e931a88abc54936
Gustavo Giráldez <ggiraldez@manas.tech>
2018-09-04 13:28:41 -0300

    Select friction region by ST_Contains, not bounding box (fixes #478)
    When selecting the friction region raster to use for friction coverages, use
    ST_Contains and not the @ operator (which only considers bounding boxes).

--- resources/planwise/sql/coverage/friction.sql
+++ resources/planwise/sql/coverage/friction.sql
@@ -1,4 +1,4 @@
 -- :name find-country-region-with-point :? :1
 -- Countries are admin_level 0 (using gadm data) and the load-friction-raster
 -- script will only clip the global raster file to the regions delimited by them
-SELECT id FROM regions WHERE "admin_level" = 0 AND :point @ "the_geom" LIMIT 1;
+SELECT id FROM regions WHERE "admin_level" = 0 AND ST_Contains("the_geom", :point) LIMIT 1;
