3fd20f81a5b4a1b690382e9d525510a112991655
Sol Sarratea <ssarratea@manas.tech>
2018-10-19 15:47:24 -0300

    Map slide when scrolling

--- src/planwise/client/projects2/components/map.cljs
+++ src/planwise/client/projects2/components/map.cljs
@@ -26,15 +26,14 @@
 
         scroll-listener (fn [this]
                           (when (safe-component-mounted? this)
-                            (let [extra-class (when (> (.-scrollY js/window) 1600) " fixed-lower-bound")]
+                            (let [extra-class (when (> (.-scrollY js/window) (.-innerHeight js/window)) " fixed-lower-bound")]
                               (dispatch-fn (str (get map-class (div-should-float? this)) extra-class)))))
 
         attach-scroll-listener (fn [this]
                                  (when-not @listener-fn
                                    (reset! listener-fn (partial scroll-listener this))
                                    (reset! top-bound (.-offsetTop (r/dom-node this)))
-                                   (.addEventListener js/window "scroll" @listener-fn)))
-        props                   (dissoc props :dispatch-fn :map-class :scroll-lower-bound)]
+                                   (.addEventListener js/window "scroll" @listener-fn)))]
     (r/create-class
      {:component-did-mount
       (fn [this]
@@ -46,7 +45,8 @@
       detach-scroll-listener
       :reagent-render
       (fn [props children]
-        [:div props children])})))
+        (let [props (dissoc props :dispatch-fn :map-class :scroll-lower-bound)]
+          [:div props children]))})))
 
 (def classes {false "static" true "floating"})
 
