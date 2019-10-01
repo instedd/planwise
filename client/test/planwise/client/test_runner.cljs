c0a4b2b9c188b8861c54247febe010195ae14c90
Gustavo Giráldez <ggiraldez@manas.tech>
2019-10-01 12:59:14 -0300

    Configuration to run CLJS tests

--- client/test/planwise/client/test_runner.cljs
+++
@@ -1,8 +0,0 @@
-(ns planwise.client.test-runner
-  (:require [cljs.test :as t :include-macros true]
-            [doo.runner :refer-macros [doo-tests]]
-            [planwise.client.utils-test]
-            [planwise.client.scenarios-test]))
-
-(doo-tests 'planwise.client.utils-test
-           'planwise.client.scenarios-test)
