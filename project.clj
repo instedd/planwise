6f35a1068d7b90e2f42675431de119d26523750f
Gustavo Giráldez <ggiraldez@manas.tech>
2019-10-03 12:22:09 -0300

    Add cljfmt aliases and run in CI

--- project.clj
+++ project.clj
@@ -90,6 +90,8 @@
 
   :aliases {"migrate"               ["with-profile" "+repl" "run" ":duct/migrator"]
             "build-icons"           ["with-profile" "+repl" "run" "-m" "planwise.tasks.build-icons"]
+            "check-format"          ["with-profile" "cljfmt" "cljfmt" "check"]
+            "fix-format"            ["with-profile" "cljfmt" "cljfmt" "fix"]
             "import-population"     ["with-profile" "+repl" "run" "-m" "planwise.tasks.import-population"]}
 
   :profiles
