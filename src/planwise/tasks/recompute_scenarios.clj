c756c9ba931c659d36ead65919f2cbe7d1304d52
Juan Blandini <jblandini@manas.com.ar>
2019-12-06 10:18:37 -0300

    Final commit for #565. Recomputing all the scenarios to avoid the described error

--- src/planwise/tasks/recompute_scenarios.clj
+++ src/planwise/tasks/recompute_scenarios.clj
@@ -1,15 +1,42 @@
 (ns planwise.tasks.recompute-scenarios
   (:gen-class)
   (:require [clojure.java.io :as io]
+            [clojure.java.jdbc :as jdbc]
             [duct.core :as duct]
             [integrant.core :as ig]
             [planwise.config]
+            [planwise.boundary.engine :as engine]
+            [planwise.component.scenarios :as scenarios]
             [taoensso.timbre :as timbre]
             [planwise.boundary.projects2 :as p2]))
 
 (timbre/refer-timbre)
-
 (duct/load-hierarchy)
+
+
+(defn recompute-scenario
+  [project store {:keys [id]}]
+  (scenarios/compute-scenario id {:store store :project project}))
+
+(defn recompute-scenarios
+  [projects2 store [project-id scenarios]]
+  (let [project    (p2/get-project projects2 project-id)]
+    (dorun (pmap (partial recompute-scenario project store) scenarios))))
+
+(defn recompute-scenario-groups
+  [groups store projects2]
+  (dorun (pmap (partial recompute-scenarios projects2 store) groups)))
+
+(defn recompute-initial-scenario
+  [projects2 store {:keys [id project-id] :as scenario}]
+  (let [project    (p2/get-project projects2 project-id)]
+    (scenarios/compute-initial-scenario id {:store store :project project})))
+
+
+(defn recompute-initial-scenarios
+  [initial-scenarios store projects2]
+  (dorun (map (partial recompute-initial-scenario projects2 store) initial-scenarios)))
+
 
 (defn -main [& args]
   ;; Logging configuration for production
@@ -17,13 +44,24 @@
                          :ns-blacklist ["com.zaxxer.hikari.*"
                                         "org.apache.http.*"
                                         "org.eclipse.jetty.*"]})
-  (let [config (-> (io/resource "planwise/cli.edn")
-                   duct/read-config
-                   duct/prep)
-        _      (println "Initializing system")
-        system (ig/init config)]
+  (let [config    (-> (io/resource "planwise/cli.edn")
+                      duct/read-config
+                      duct/prep)
+        _         (println "Starting system")
+        system    (ig/init config)]
+
     (try
-      (println (p2/list-projects (:planwise.component/projects2 system) 1))
+      (let [projects2               (:planwise.component/projects2 system)
+            store                   (:planwise.component/scenarios system)
+            database                (:spec (second (ig/find-derived-1 system :duct.database/sql)))
+            project                 (p2/get-project projects2 11)
+            initial-scenarios       (jdbc/query database ["SELECT id, label, \"project-id\" FROM scenarios WHERE label = 'initial'"])
+            scenarios               (jdbc/query database ["SELECT id, label, \"project-id\" FROM scenarios WHERE label <> 'initial'"])
+            scenarios               (group-by :project-id scenarios)]
+
+        (recompute-initial-scenarios initial-scenarios store projects2)
+        (recompute-scenario-groups scenarios store projects2))
+
       (finally
         (println "Shutting down system")
         (ig/halt! system)))))
