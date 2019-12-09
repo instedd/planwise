09f5f601dd1e300dcc458f8f848df1cb55562b53
Juan Blandini <jblandini@manas.com.ar>
2019-12-09 13:44:59 -0300

    Removing the new raster when removing unused scenario files For #567

--- src/planwise/component/scenarios.clj
+++ src/planwise/component/scenarios.clj
@@ -55,6 +55,8 @@
     (io/delete-file (io/file (str "data/" raster ".tif")))
     (io/delete-file (io/file (str "data/" raster ".map.tif")))
     (io/delete-file (io/file (str "data/" raster ".coverage.tif")))
+    (io/delete-file (io/file (str "data/" raster ".base-demand.tif")))
+
     (let [old-provider-ids (set (keys (:new-providers-geom scenario)))
           new-provider-ids (set (keys (:new-providers-geom scenario-result-after-computation)))
           removed-ids  (set/difference old-provider-ids new-provider-ids)]
