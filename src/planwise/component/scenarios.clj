a1852e76c6a839984b5bb931daa84ab2e5bd768b
Gustavo Giráldez <ggiraldez@manas.tech>
2019-12-13 18:07:04 -0300

    Silently ignore errors while removing old raster files

--- src/planwise/component/scenarios.clj
+++ src/planwise/component/scenarios.clj
@@ -52,9 +52,9 @@
 (defn remove-unused-scenario-files
   [{:keys [id raster] :as scenario} scenario-result-after-computation]
   (when (some? raster)
-    (io/delete-file (io/file (str "data/" raster ".tif")))
-    (io/delete-file (io/file (str "data/" raster ".map.tif")))
-    (io/delete-file (io/file (str "data/" raster ".coverage.tif")))
+    (io/delete-file (io/file (str "data/" raster ".tif")) :silent)
+    (io/delete-file (io/file (str "data/" raster ".map.tif")) :silent)
+    (io/delete-file (io/file (str "data/" raster ".coverage.tif")) :silent)
     (let [old-provider-ids (set (keys (:new-providers-geom scenario)))
           new-provider-ids (set (keys (:new-providers-geom scenario-result-after-computation)))
           removed-ids  (set/difference old-provider-ids new-provider-ids)]
