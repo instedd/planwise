daf208d5d3ddb9e6b9875caf65609ff42807a908
Juan Blandini <jblandini@manas.com.ar>
2020-01-30 11:58:48 -0300

    Fix for #572

--- client/src/planwise/client/scenarios/changeset.cljs
+++ client/src/planwise/client/scenarios/changeset.cljs
@@ -50,5 +50,5 @@
 (defn- suggestion-listing-component
   [suggestions]
   [:div {:class-name "scroll-list suggestion-list"}
-   (map (fn [suggestion] [suggestion-row {:key (str "suggestion-action" (:ranked suggestion))} suggestion])
+   (map (fn [suggestion] [suggestion-row {:key (str "suggestion-action" (:name suggestion) (:ranked suggestion))} suggestion])
         suggestions)])
