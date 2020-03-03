55452c4314c3a2d834570a7eba0700998ece36af
Gustavo Giráldez <ggiraldez@manas.tech>
2020-03-03 12:39:45 -0300

    Update templates.clj

--- src/planwise/configuration/templates.clj
+++ src/planwise/configuration/templates.clj
@@ -5,25 +5,25 @@
   [{:description "Plan facilities based on ground access"
     :icon "directions_walk"
     :key "plan"
-    :defaults {:name "ground"
-               :config {:coverage {:filter-options {:driving-time 90}}
+    :defaults {:name "Improve the coverage of meningitis rapid test"
+               :config {:coverage {:filter-options {:driving-time 90}} ;How to define Region?
                         :demographics {:target 12
-                                       :unit-name "humans"}
-                        :actions {:budget 123123
+                                       :unit-name "suspected cases of meningitis"}
+                        :actions {:budget 500000
                                   :build [{:id "build-0"
-                                           :capacity 123
-                                           :investment 456}
+                                           :capacity 100
+                                           :investment 90000}
                                           {:id "build-1"
-                                           :capacity 333
-                                           :investment 444}]
+                                           :capacity 300
+                                           :investment 150000}]
                                   :upgrade [{:id "upgrade-0"
-                                             :capacity 123
-                                             :investment 456}
+                                             :capacity 100
+                                             :investment 10000}
                                             {:id "upgrade-1"
-                                             :capacity 333
-                                             :investment 444}]}}
+                                             :capacity 300
+                                             :investment 40000}]}}
 
-               :source-set-id 2
+               :source-set-id nil
                :region-id nil
                :provider-set-id nil}}
    {:description "Plan diagonostic devices & sample referrals"
