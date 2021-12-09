e1712fe1021d65714cd0311f4f63c4198872d3d5
Gustavo Giráldez <ggiraldez@manas.tech>
2021-12-09 18:10:18 -0300

    Remove obsolete code used for naming scenarios

--- src/planwise/util/str.clj
+++ src/planwise/util/str.clj
@@ -10,35 +10,3 @@
     (trim-to-int s)
     (catch Exception e nil)))
 
-(defn next-char [letter]
-  (-> letter int inc char))
-
-(defn next-alpha-name
-  [name]
-  (loop [prefix name
-         sufix   ""]
-    (let [prefix* (apply str (drop-last prefix))
-          last    (last prefix)]
-      (cond (empty? prefix) (str \A sufix)
-            (not= last \Z) (str prefix* (next-char last) sufix)
-            :else (recur prefix* (str \A sufix))))))
-
-(defn extract-name-and-copy-number
-  [s]
-  (let [match      (re-matches #"\A(?<name>.*)(?<copy>\d+?)\Z" s)
-        name-group (get match 1)
-        copy-group (last match)]
-    (cond
-      (nil? match)      {:name s :copy 0}
-      :else             {:name name-group :copy (Integer. copy-group)})))
-
-(defn next-name
-  [col]
-  (let [names-and-numbers (map extract-name-and-copy-number col)
-        first-name (:name (first names-and-numbers))
-        next-copy  (->> names-and-numbers
-                        (filter #(= (:name %) first-name))
-                        (map :copy)
-                        (apply max)
-                        (inc))]
-    (str first-name next-copy)))
